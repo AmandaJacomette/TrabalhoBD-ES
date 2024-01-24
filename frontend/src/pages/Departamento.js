88b7e3b71804c2e359c7e8a7e453d38ad3b07b8d
sakikout <bedalfior@gmail.com>
2024-01-24 16:13:23 -0300

    Adding tables to components
    Components changed: Recursos Humanos, Encomendas, Departamento

--- frontend/src/pages/Departamento.js
+++ frontend/src/pages/Departamento.js
@@ -1,12 +1,117 @@
-import React, { useState } from 'react';
-import '../App.css';
+import React, { useState, useEffect, useContext } from 'react';
+import '../components/style/style.css'
+
 import logo from "../img/logo.png"
 import logoSimples from "../img/logoSimples.png"
+import StoreContext from '../components/Store/Context';
+
+
+function createRandomUsers(count = 5) {
+  const users = [];
+  
+  for (let i = 0; i < count; i++) {
+    users.push({
+      userId: i + 120,
+      cpf: "123456789 -0" + i,
+      name: "Funcionario " + i,
+      funcao : "Operador",
+      salario: 1417.25,
+      dataInicio: '01-01-2024',
+      horaIntervalo: '12:30'
+    });
+  }
+
+  return users;
+}
 
 function Departamento({userData}){
+  const [tableData, setTableData] = useState([]);
+  const { setToken, token } = useContext(StoreContext);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      /*
+      const response = await fetch("https://cities-qd9i.onrender.com/agents");
+      const agents = await response.json();
+      
+      setTableData(agents);
+      */
+     console.log(tableData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleCreateUsers = () => {
+    const newUsers = createRandomUsers()
+    setTableData([...tableData, ...newUsers])
+  }
+
+  const handleClearUsers = () => {
+    setTableData([]);
+  }
 
     return (
-        <div >
+        <div className="mform">
+        <div className = "text">Funcionários</div>
+      <button className= "update-btn" onClick={handleCreateUsers}>Create Users</button>
+      <button className= "delete-btn" onClick={handleClearUsers}>Delete Users</button>
+      <div class="table">
+      <div class="table-header">
+          
+      <div class="header__item">
+        <a id="idFunc" class="filter__link">
+          ID</a>
+        </div>
+        <div class="header__item">
+          <a id="cpf" class="filter__link filter__link--number" >
+            CPF
+          </a>
+          </div>
+          <div class="header__item">
+          <a id="nome" class="filter__link filter__link--number">
+            Funcionário</a>
+          </div>
+          <div class="header__item">
+          <a id="funcao" class="filter__link filter__link--number">
+            Função</a>
+          </div>
+          <div class="header__item">
+          <a id="salario" class="filter__link filter__link--number">
+            Salário</a>
+          </div>
+          <div class="header__item">
+          <a id="dataInicio" class="filter__link filter__link--number">
+            Data Início</a>
+          </div>
+          <div class="header__item">
+          <a id="horaIntervalo" class="filter__link filter__link--number">
+            Hora de Intervalo</a>
+          </div>
+
+        </div>
+        <div class="table-content">
+          {
+            tableData.map((obj) => {
+              return (
+                <div class="table-row">
+                  <div class="table-data">{obj.userId}</div>
+                  <div class="table-data">{obj.cpf}</div>
+                  <div class="table-data">{obj.name}</div>
+                  <div class="table-data">{obj.funcao}</div>
+                  <div class="table-data">{obj.salario}</div>
+                  <div class="table-data">{obj.dataInicio}</div>
+                  <div class="table-data">{obj.horaIntervalo}</div>
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
         </div>
   );
 }
--- frontend/src/pages/RecursosHumanos.js
+++ frontend/src/pages/RecursosHumanos.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 import {withRouter} from 'react-router-dom';
@@ -8,18 +8,38 @@ import axios from 'axios';
 import logo from "../img/logo.png"
 import Popup from '../components/popuplogin';
 import '../components/style/style.css'
+import StoreContext from '../components/Store/Context';
 
+function createRandomChamados(count = 5) {
+  const chamados = [];
+  
+  for (let i = 0; i < count; i++) {
+    chamados.push({
+      idFunc: '200' + i,
+      nome: 'Funcionario 1',
+      departamento: '2',
+      titulo: 'Chamado Teste ' + i,
+      assunto: 'Assunto do Chamado ' + i,
+      status: 'Encaminhado'
+    });
+  }
+
+  return chamados;
+}
 
 function RecursosHumanos({userData}){
     //const history = useNavigate();
     const navigate = useNavigate();
+    const [tableData, setTableData] = useState([]);
+    const { setToken, token } = useContext(StoreContext);
     
     const[formData, setChamados] = useState({
             idFunc: '2001',
             nome: 'Funcionario',
             departamento: '2',
             titulo: 'Chamado Teste',
-            assunto: 'Assunto do Chamado'
+            assunto: 'Assunto do Chamado',
+            status: ' '
     });
     
     const handleInputChange = (event) => {
@@ -28,6 +48,25 @@ function RecursosHumanos({userData}){
           ...prevData,
           [name]: value,
         }));
+    };
+
+    
+    useEffect(() => {
+      fetchData();
+    }, []);
+  
+    const fetchData = async () => {
+      try {
+        /*
+        const response = await fetch("https://cities-qd9i.onrender.com/agents");
+        const agents = await response.json();
+        
+        setTableData(agents);
+        */
+       console.log(tableData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
 
     /*
@@ -55,6 +94,16 @@ function RecursosHumanos({userData}){
         console.error('Erro ao enviar dados:', error);
       });*/
   };
+
+  const handleClearChamados = () => {
+    setTableData([]);
+  }
+
+const handleCreateChamados = () => {
+    const newUsers = createRandomChamados()
+    setTableData([...tableData, ...newUsers])
+}
+
 
     return (
       <div className="mform">
@@ -144,7 +193,64 @@ function RecursosHumanos({userData}){
             </div>
         </form>
         <div className='Chamados'>
+        <div className="mtable">
           <div class = "text">Chamados</div>
+      <button className= "update-btn" onClick={handleCreateChamados}>Criar</button>
+      <button className= "delete-btn" onClick={handleClearChamados}>Deletar</button>
+      {
+
+      }
+      <div class="table">
+      <div class="table-header">
+          
+      <div class="header__item">
+        <a id="idFunc" class="filter__link">
+          ID</a>
+        </div>
+        <div class="header__item">
+          <a id="nome" class="filter__link filter__link--number" >
+           Nome
+          </a>
+          </div>
+          <div class="header__item">
+          <a id="departamento" class="filter__link filter__link--number">
+            Departamento</a>
+          </div>
+          <div class="header__item">
+          <a id="titulo" class="filter__link filter__link--number">
+            Titulo</a>
+          </div>
+          <div class="header__item">
+          <a id="assunto" class="filter__link filter__link--number">
+            Assunto</a>
+          </div>
+          <div class="header__item">
+          <a id="status" class="filter__link filter__link--number">
+           Status</a>
+          </div>
+
+
+        </div>
+        <div class="table-content">
+          {
+            tableData.map((obj) => {
+              return (
+                <div class="table-row">
+                  <div class="table-data">{obj.idFunc}</div>
+                  <div class="table-data">{obj.nome}</div>
+                  <div class="table-data">{obj.departamento}</div>
+                  <div class="table-data">{obj.titulo}</div>
+                  <div class="table-data">{obj.quantidade}</div>
+                  <div class="table-data">{obj.assunto}</div>
+                  <div class="table-data">{obj.status}</div>
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
+        </div>
+
         </div>
       </div>
         
