bbc639bdf3f45d63dc29f9ef8b7444e1b5154310
amanda jacomette <amandajacomett@gmail.com>
2024-02-01 23:22:12 -0300

    Adicionando frontend funcionarios

--- frontend/src/pages/Departamento.js
+++ frontend/src/pages/Departamento.js
@@ -4,6 +4,8 @@ import '../components/style/style.css'
 import logo from "../img/logo.png"
 import logoSimples from "../img/logoSimples.png"
 import StoreContext from '../components/Store/Context';
+import Popup from '../components/Popup';
+import axios from 'axios';
 
 
 function createRandomUsers(count = 5) {
@@ -26,8 +28,26 @@ function createRandomUsers(count = 5) {
 
 function Departamento({userData}){
   const [tableData, setTableData] = useState([]);
+  const [buttonPopup, setButtonPopup] = useState(false);
+  const [buttonDeletePopup, setDeletePopup] = useState(false);
 
   const { setToken, token } = useContext(StoreContext);
+
+  const[formData, setFunc] = useState({
+    nome: '',
+    cpf: '',
+    senha: '',
+    salario: 0,
+    intervalo: ''
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFunc((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+};
 
   useEffect(() => {
     fetchData();
@@ -47,21 +67,60 @@ function Departamento({userData}){
     }
   };
 
-  const handleCreateUsers = () => {
-    const newUsers = createRandomUsers()
-    setTableData([...tableData, ...newUsers])
-  }
-
   const handleClearUsers = () => {
     setTableData([]);
   }
 
+  const handleCreateUsers = () => {
+    setButtonPopup(true);
+  }
+
+  const handleDelete = () => {
+    setDeletePopup(true);
+  }
+
+  const handleSubmitModal = (event) => {
+    event.preventDefault();
+    console.log(formData.id);
+    console.log(formData.status);
+
+    axios.post('http://127.0.0.1:5000/api/criaFuncionario', formData)
+      .then(response => {
+        console.log('Resposta do servidor:', response.data);
+        setButtonPopup(false);
+        //handleClearEnc()
+        //handleCreateEnc(event)
+      })
+      .catch(error => {
+        console.error('Erro ao enviar dados:', error);
+      });
+  }
+
+  const handleSubmitDelete = (event) => {
+    event.preventDefault();
+    console.log(formData.id);
+    console.log(formData.status);
+
+    axios.post('http://127.0.0.1:5000/api/deletaEncomenda', formData)
+      .then(response => {
+        console.log('Resposta do servidor:', response.data);
+        setDeletePopup(false);
+        //handleClearEnc()
+        //handleCreateEnc(event)
+      })
+      .catch(error => {
+        console.error('Erro ao enviar dados:', error);
+      });
+
+      
+  }
 
     return (
         <div className="mform">
         <div className = "text">Funcionários</div>
-      <button className= "update-btn" onClick={handleCreateUsers}>Create Users</button>
-      <button className= "delete-btn" onClick={handleClearUsers}>Delete Users</button>
+        
+      <button className= "update-btn" onClick={handleCreateUsers}>Criar</button>
+      <button className= "delete-btn" onClick={handleDelete}>Deletar</button>
 
       <div class="table">
       <div class="table-header">
@@ -110,7 +169,93 @@ function Departamento({userData}){
           }
         </div>
       </div>
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+          <div className='container-modal'>
+            <div className="text-modal">Criar Funcionario</div>
+            <form onSubmit={handleSubmitModal}>
+              <div class="input-modal">
+                <label className='modalLabel' for="cdprod">
+                  Nome
+                </label>
+                <input 
+                      name="nome" 
+                      className='dadosEncomenda' 
+                      value={formData.nome}
+                      onChange={handleInputChange} required/>
+              </div>
 
+              <div class = "input-modal">
+                <label className='modalLabel' for="quantidade">
+                  CPF
+                </label>
+                <input 
+                      name="cpf" 
+                      className='dadosEncomenda' 
+                      value={formData.cpf}
+                      onChange={handleInputChange} required />
+              </div>
+
+              <div class = "input-modal">
+                <label className='modalLabel' for="quantidade">
+                  Senha
+                </label>
+                <input 
+                      name="senha" 
+                      className='dadosEncomenda' 
+                      value={formData.senha}
+                      onChange={handleInputChange} required />
+              </div>
+
+              <div class = "input-modal">
+                <label className='modalLabel' for="quantidade">
+                  Salario
+                </label>
+                <input 
+                      name="salario" 
+                      className='dadosEncomenda' 
+                      value={formData.salario}
+                      onChange={handleInputChange} required />
+              </div>
+
+              <div class = "input-modal">
+                <label className='modalLabel' for="quantidade">
+                  Intervalo
+                </label>
+                <input 
+                      name="intervalo" 
+                      className='dadosEncomenda' 
+                      value={formData.intervalo}
+                      onChange={handleInputChange} required />
+              </div>
+
+              <button className= "modalButton" 
+              type = "submit"
+             >Atualizar</button>
+              </form> 
+          </div>
+        </Popup>
+
+        <Popup trigger={buttonDeletePopup} setTrigger={setDeletePopup}>
+          <div className='container-modal'>
+            <div className="text-modal">Deleta Encomenda</div>
+            <form onSubmit={handleSubmitDelete}>
+              <div class="input-modal">
+              <label className='modalLabel' for="cdprod">
+                  ID
+                </label>
+                  <input 
+                        name="id" 
+                        className='dadosEncomenda' 
+                        value={formData.id}
+                        onChange={handleInputChange} required/>
+                  
+                </div>
+              <button className= "modalButton" 
+              type = "submit"
+             >Excluir</button>
+              </form> 
+          </div>
+        </Popup>
       
         </div>
   );
